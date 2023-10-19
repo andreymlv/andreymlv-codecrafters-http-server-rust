93970b84a78fd70a484349a415e7fcca973f68cc
andreymlv <ndrmlv@ya.ru>
2023-10-19 11:42:12 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
-use std::{net::TcpListener, io::{Read, Write}, vec};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+    vec,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let mut buf = vec![];
-                stream.read_to_end(&mut buf).unwrap();
-                let respond = "HTTP/1.1 200 OK\r\n\r\n";
-                stream.write_all(respond.as_bytes()).unwrap();
+            Ok(stream) => {
+                handle_client(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_client(mut stream: std::net::TcpStream) {
+    println!("accepted new connection");
+    let mut buf = vec![];
+    stream.read(&mut buf).unwrap();
+    let respond = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(respond.as_bytes()).unwrap();
 }
