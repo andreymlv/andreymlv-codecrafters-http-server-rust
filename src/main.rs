ee031a88af1ef6ac0005aa4c45c6c43c334f5248
andreymlv <ndrmlv@ya.ru>
2023-10-21 12:54:14 +0300

    parse request and headers

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,123 @@
 use anyhow::Result;
 use nom::branch::alt;
-use nom::bytes::complete::{take_till, take_until};
-use nom::character::complete::{alpha0, alpha1, char};
-use nom::combinator::value;
-use nom::multi::many0;
-use nom::sequence::separated_pair;
+use nom::bytes::complete::take_while1;
+use nom::character::complete::char;
+use nom::multi::many1;
+use nom::sequence::{delimited, pair, preceded, terminated};
 use nom::{bytes::complete::tag, IResult};
 use std::{
-    // collections::HashMap,
     io::{Read, Write},
     net::TcpListener,
     str,
 };
 
-#[derive(Debug, Clone)]
-enum Method {
-    GET,
-    POST,
+#[derive(Debug)]
+struct Request<'a> {
+    method: &'a [u8],
+    uri: &'a [u8],
+    version: &'a [u8],
 }
 
-// #[derive(Debug)]
-// struct StartLine {
-//     method: Method,
-//     path: String,
-//     version: String,
-// }
+#[derive(Debug)]
+struct Header<'a> {
+    name: &'a [u8],
+    value: Vec<&'a [u8]>,
+}
 
-// #[derive(Debug)]
-// struct Request {
-//     start_line: StartLine,
-//     // headers: HashMap<String, String>,
-//     // body: Option<String>,
-// }
+fn is_token(c: u8) -> bool {
+    match c {
+        128..=255 => false,
+        0..=31 => false,
+        b'(' => false,
+        b')' => false,
+        b'<' => false,
+        b'>' => false,
+        b'@' => false,
+        b',' => false,
+        b';' => false,
+        b':' => false,
+        b'\\' => false,
+        b'"' => false,
+        b'/' => false,
+        b'[' => false,
+        b']' => false,
+        b'?' => false,
+        b'=' => false,
+        b'{' => false,
+        b'}' => false,
+        b' ' => false,
+        _ => true,
+    }
+}
+
+fn not_line_ending(c: u8) -> bool {
+    c != b'\r' && c != b'\n'
+}
+
+fn is_space(c: u8) -> bool {
+    c == b' '
+}
+
+fn is_not_space(c: u8) -> bool {
+    c != b' '
+}
+fn is_horizontal_space(c: u8) -> bool {
+    c == b' ' || c == b'\t'
+}
+
+fn is_version(c: u8) -> bool {
+    c >= b'0' && c <= b'9' || c == b'.'
+}
+
+fn line_ending(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    alt((tag("\r\n"), tag("\n")))(input)
+}
+
+fn request_line<'a>(input: &'a [u8]) -> IResult<&'a [u8], Request<'a>> {
+    let (input, method) = take_while1(is_token)(input)?;
+    let (input, _) = take_while1(is_space)(input)?;
+    let (input, url) = take_while1(is_not_space)(input)?;
+    let (input, _) = take_while1(is_space)(input)?;
+    let (input, version) = http_version(input)?;
+    let (input, _) = line_ending(input)?;
+    Ok((
+        input,
+        (Request {
+            method,
+            uri: url,
+            version,
+        }),
+    ))
+}
+
+fn http_version(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    preceded(tag("HTTP/"), take_while1(is_version))(input)
+}
+
+fn message_header_value(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    delimited(
+        take_while1(is_horizontal_space),
+        take_while1(not_line_ending),
+        line_ending,
+    )(input)
+}
+
+fn message_header<'a>(input: &'a [u8]) -> IResult<&'a [u8], Header<'a>> {
+    let (rest, name) = take_while1(is_token)(input)?;
+    let (rest, _) = char(':')(rest)?;
+    let (rest, values) = many1(message_header_value)(rest)?;
+    Ok((
+        rest,
+        Header {
+            name,
+            value: values,
+        },
+    ))
+}
+
+fn request<'a>(input: &'a [u8]) -> IResult<&'a [u8], (Request<'a>, Vec<Header<'a>>)> {
+    terminated(pair(request_line, many1(message_header)), line_ending)(input)
+}
 
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221")?;
@@ -50,16 +136,19 @@ fn main() -> Result<()> {
 }
 
 fn handle_client(mut stream: std::net::TcpStream) -> Result<()> {
-    let mut request = [0u8; 1024];
+    let mut request = [0u8; 2048];
     let read = stream.read(&mut request)?;
-    let request = str::from_utf8(&request[..read])?;
-    let (rest, _) = parse_method(&request).unwrap();
-    let (_, path) = parse_path(&rest).unwrap();
+    if read >= request.len() {
+        stream.write_all(b"HTTP/1.1 414 URI Too Long\r\n\r\n")?;
+        return Ok(());
+    }
+    let (_, request) = request_line(&request[..read]).unwrap();
+    let path = str::from_utf8(request.uri)?;
     if path.starts_with("/echo/") {
         let echo = &path[6..];
         let len = echo.len();
         let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len}\r\n\r\n{echo}\r\n"
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len}\r\n\r\n{echo}"
         );
         stream.write_all(response.as_bytes())?;
     } else if path == "/" {
@@ -70,15 +159,4 @@ fn handle_client(mut stream: std::net::TcpStream) -> Result<()> {
         stream.write_all(response)?;
     }
     Ok(())
-}
-
-fn parse_method(input: &str) -> IResult<&str, Method> {
-    alt((
-        value(Method::GET, tag("GET ")),
-        value(Method::POST, tag("POST ")),
-    ))(input)
-}
-
-fn parse_path(input: &str) -> IResult<&str, &str> {
-    take_till(|c| c == ' ')(input)
 }
